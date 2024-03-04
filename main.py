ea0875d6538f2ecf889652ae242ac48f0a1f17a9
Joey <37638087+Yunikuna-Joey@users.noreply.github.com>
2024-03-04 00:14:45 -0800

    [wip] looking at packages

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-from lumibot.brokers import Alpaca 
-from lumibot.backtesting import YahooDataBacktesting
-from lumibot.strategies.strategy import Strategy
+#* Lumi bot: algo trading framework 
+from lumibot.brokers import Alpaca #* broker
+from lumibot.backtesting import YahooDataBacktesting    #* framework for backtesting 
+from lumibot.strategies.strategy import Strategy        #* bot platform
+from lumibot.traders import Trader                      #* Deployment abilities
+
